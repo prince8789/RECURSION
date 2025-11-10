0470d5b5235e48dcbfa8f2de5d4f2614cf86eeef
prince8789 <princekumarbgu9097@gmail.com>
2025-11-10 15:17:49 +0530

    temp

---
+++ OOPS/Poo.java
@@ -0,0 +1,10 @@
+package OOPS;
+
+class Poo {
+   public static void main(String[] args) {
+      System.out.println("Hello world");
+      Student st = new Student();
+      st.name = "op";
+      System.out.println(st.name);
+   }
+} 
