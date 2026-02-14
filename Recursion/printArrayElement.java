dcf81e6e17e87c0bb21da362d8cdffca7fa17bad
Prince Kumar <princekumargbu8789@gmail.com>
2026-02-14 14:47:45 +0530

    display array , find maximum element in array , sum of all element in array using recursion

--- Recursion/printArrayElement.java
+++ Recursion/printArrayElement.java
@@ -1,14 +1,29 @@
 public class printArrayElement {
     static void Display(int[] arr, int idx){
-        if(idx==0){
-            System.out.print(arr[idx]+" ");
+        if(idx==arr.length){
+            return;
         }
-        Display(arr,idx-1);
         System.out.print(arr[idx]+" ");
+        Display(arr,idx+1);
+        
     }
+
+    static int PrintMaxValueinArr(int[]arr ,int idx){
+        if(idx==arr.length-1) return arr[idx];
+        int small_answer = PrintMaxValueinArr(arr, idx+1);
+        return Math.max(small_answer,arr[idx]);
+    }
+     static int sumofArr(int[] arr, int idx){
+        if(idx==arr.length-1) return arr[idx];
+        return arr[idx]+sumofArr(arr,idx+1);
+     }
     public static void main(String[] args) {
-        int[] arr ={1,3,5,7,8,4,5,7,9};
-        Display(arr, arr.length);
+        int[] arr ={1,3,5,7,8,4,5,7,9,98,7,29,36,80,66};
+        Display(arr,0);
+        System.out.println();
+        System.out.println("the maximum element in this array is ");
+        System.out.println(PrintMaxValueinArr(arr,0));
+        System.out.println(sumofArr(arr, 0));
     }
     
 }
